42dfbbd603d117d21cdf4725a7bc8e1a07cc54da
Juan Espinosa <juanjavierzerofour@gmail.com>
2021-05-23 17:45:45 -0300

    implementando "calculoDeError()" y "expresion()"

--- src/main/medicionMayorADiez.java
+++ src/main/medicionMayorADiez.java
@@ -1,17 +1,39 @@
 package main;
 
+import java.util.List;
+
 public class medicionMayorADiez extends valorPromedio implements IErrorAbsoluto {
+	
+	private double promedio;
+	private List<Double> medicion;
+	
+	public medicionMayorADiez(List<Double> medicion) {
+		this.medicion = medicion;
+		promedio = calculoPromedio(medicion);
+	}
 
 	@Override
 	public double calculoDeError(double promedio) {
-		// TODO Auto-generated method stub
-		return 0;
+		double longitud = medicion.size();
+		double valor = 0;
+		double resultado = Math.pow(valor / longitud, 2);
+		for(double instancia : medicion) {
+			valor += Math.pow(promedio - instancia, 2);
+		}
+		return resultado;
+	}
+
+	public double getPromedio() {
+		return promedio;
+	}
+
+	public List<Double> getMedicion() {
+		return medicion;
 	}
 
 	@Override
 	public String expresion() {
-		// TODO Auto-generated method stub
-		return null;
+		return "El resultado es " + getPromedio() + "+- " + calculoDeError(getPromedio());
 	}
 
 }
